7fa0c25f00e32f0ed8ff56b9c1aa362f98c5e82f
tmunzer <thibaut.munzer@inria.fr>
2015-06-17 23:07:04 +0200

    Api add auto_remove

--- web_asker_api/src/web_asker/web_asker.py
+++ web_asker_api/src/web_asker/web_asker.py
@@ -5,9 +5,10 @@ import time
 from pymongo import MongoClient
 
 class WebQuestion(object):
-    def __init__(self, mongo_db_id, questions_col):
+    def __init__(self, mongo_db_id, questions_col, auto_remove):
         self._mogo_db_id = mongo_db_id
         self._questions_col = questions_col
+        self._auto_remove = auto_remove
 
     def answered(self):
         return self._questions_col.find_one({
@@ -18,7 +19,8 @@ class WebQuestion(object):
             result = self._questions_col.find_one({
                 "answer" : {"$exists" : True}, "_id" : self._mogo_db_id})
             if result is not None:
-                self._alive = False
+                if self._auto_remove:
+                    self.remove()
                 return result["answer"]
             time.sleep(0.1)
 
@@ -32,12 +34,12 @@ class WebAsker(object):
         self._questions_col = client[db_name]["questions"]
         self._mogo_db_id = None
 
-    def ask(self, question, answer_list):
+    def ask(self, question, answer_list, auto_remove=True):
         question_id = self._questions_col.insert({
             "text" : question,
             "answer_candidates" : [{"text" : a} for a in answer_list],
             })
-        return WebQuestion(question_id, self._questions_col)
+        return WebQuestion(question_id, self._questions_col, auto_remove)
 
     def clear_all(self):
         self._questions_col.remove()
