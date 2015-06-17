20f65e093c1cd176cc06aad6a70b43c9921194ca
tmunzer <thibaut.munzer@inria.fr>
2015-06-17 20:36:24 +0200

    Update css + html

--- web_asker_api/src/web_asker/web_asker.py
+++ web_asker_api/src/web_asker/web_asker.py
@@ -26,10 +26,10 @@ class WebQuestion(object):
         self._questions_col.remove({"_id" : self._mogo_db_id})
 
 class WebAsker(object):
-    def __init__(self, mongo_db_adress):
+    def __init__(self, mongo_db_adress, db_name="web_asker_db"):
         client = MongoClient(mongo_db_adress)
 
-        self._questions_col = client["web_asker_db"]["questions"]
+        self._questions_col = client[db_name]["questions"]
         self._mogo_db_id = None
 
     def ask(self, question, answer_list):
